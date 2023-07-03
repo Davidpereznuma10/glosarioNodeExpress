69441d01a4c060e0124508bb41dc121c4dadd457
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-03 18:55:20 -0500

    feat :sparkles: Se agrega el spread y values
    -Spread : Sirve para poder añadir data a un array de elementos utilizando (...) antes del elemento creado, despues de agrega el valor que quieres agregar para que se pueda añadir commo en el ejemplo.
    -values: sirve par apoder determinar valores despues de un determinado parametro pasado en un array esto sirve para añadir varios valores y que no afecte el resultado ( es recomendo utilizarlo solo cuando se pasan hasta 3 parametros).

--- src/04-rest-spread.js
+++ src/04-rest-spread.js
@@ -16,3 +16,24 @@ let user = {username: 'David', age:23};
 let {username, age} = user;
 
 console.log(username, age);
+
+/**
+ * Spread Operator
+ */
+
+let person = {name : "David", age:23};
+let country = "Col";
+let data = {...person, country}
+
+console.log( data);
+
+/**
+ * Values
+ */
+
+function sum(num, ...values){
+console.log(values);
+console.log(num + values[2]);
+}
+
+sum(5,2,6,7)
