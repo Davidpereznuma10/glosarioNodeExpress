20cd7070340487da366babc84324541b067c77e2
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-03 17:58:28 -0500

    feat: :sparkles: Valores de variables
    - var nos permite acceder al valor de la varuiable desde fuera de la funcion que se esta modificando, adeamas de poder modificar su valor; let nos permite modificar su valor pero no nos permite acceder a ella desde fuera de la funcion que estemos utilizando; por ultimo tenemos a const que no permite modificar su valor ni tampoco tiene un alcance global.

---
+++ src/00-let-const.js
@@ -0,0 +1,25 @@
+var name = "David";
+name = "Juan";
+console.log(name);
+
+let fruit = "kiwi";
+fruit = "banana";
+console.log(fruit);
+
+
+const pet = "dog";
+pet = "cat"; 
+console.log(pet);
+
+const fruites = ()=>{
+    if(true){
+        var fruit1 = "Aplle";
+        let fruit2 = "ñame";
+        const fruit3 = "naranja";
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruites();
