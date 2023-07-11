ad76a63be6e884bafd0a4c559e5afa5d5908420d
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-11 16:26:21 -0500

    feat :sparkles: replaceAll
    Esto nos ayudaa buscar paralabras especificas o letras y remplazarlas en cualquier parte del string que tengamos.

---
+++ src/01-replaceall.js
@@ -0,0 +1,5 @@
+const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus harum minus dolore quia unde quasi saepe velit nam eligendi vero, sunt nemo voluptas excepturi maxime repellat id veniam aspernatur quas.';
+
+const replace =string.replaceAll("e", "(-:-)") ; 
+
+console.log(replace);
