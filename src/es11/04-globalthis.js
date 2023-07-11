77aa3dcbbe72b5cdffc5ada1df1b1e7d87804715
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-11 09:14:15 -0500

    feat :sparkles: global
    Esto nos ayuda a mostrar todas nuestras configuracion del navegador y node, esto ayuda para poder acceder a nuevas propiedades.

---
+++ src/es11/04-globalthis.js
@@ -0,0 +1,7 @@
+console.log(globalThis);
+console.log('-------------------------------');
+console.log(global);
+console.log('-------------------------------');
+console.log(self);
+console.log('-------------------------------');
+console.log(window);
